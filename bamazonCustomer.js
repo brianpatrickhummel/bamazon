45ff4d2576f4a701e9040fb92aac973427551956
Brian Hummel <frankgucci@mac.com>
2017-07-01 21:02:55 -0500

    finished the order section and check inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,9 +21,12 @@ connection.connect(function(err) {
 
 
 var lookup = {
+  index: null,
+  product: null,
+  quantity: 0,
   displayAllProducts: function() {
     console.log("*   •   *   •   *   •   *   •   *   •   *   •   *   •");
-    console.log("\n********** Welcome to the bAMAZON Store **********\n");
+    console.log("\n********** Welcome to the Bamazon Store **********\n");
     console.log("           (LOADING STORE INVENTORY)\n");
     setTimeout(function() {
       connection.query("SELECT * FROM products", function(err, res) {
@@ -58,23 +61,71 @@ var lookup = {
             name: 'numberToPurchase'
           }
         ]).then(function(answers) {
-          var index= ((answers.itemId) - 1);
-          var product = res[index].product_name;
+          index = ((answers.itemId) - 1);
+          // console.log(index);
+          product = res[index].product_name;
+          // console.log(product);
+          lookup.quantity = answers.numberToPurchase;
+          // console.log(quantity);
           inquirer.prompt([
             {
               type: 'list',
-              message: 'Please confirm, do you wish to purchase ' + answers.numberToPurchase + ' of this product: ' + product,
+              message: 'Please confirm, do you wish to purchase (' + answers.numberToPurchase + ') of this product:\n' + product,
               choices: ['Yes', 'No'],
               name: 'whichOne'
             }
           ]).then(function(answers) {
-            
+            if (answers.whichone === 'No'){
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  message: 'What would you like to do?',
+                  choices: ['View Bamazon product list again', 'Exit'],
+                  name: 'buyOrLeave'
+                }
+              ]).then(function(answers) {
+                if (answers.buyOrLeave === 'View Bamazon product list again'){
+                  look.displayAllProducts();
+                }
+                else {
+                  console.log("Farewell!");
+                  return false;
+                }
+              });
+            }
+            else {
+              lookup.checkInventoryStock(res, product);
+            }
           });
         });
       }
     });
+  },
+  checkInventoryStock: function(res, product){
+    if (res[index].stock_quantity > lookup.quantity){
+      console.log("\n* * * * * * * * * ** * * * * * * * * * * * * * * * * * * * * * *\n")
+      var totalCost = ((lookup.quantity) * (res[index].price));
+      inquirer.prompt([
+        {
+          type: 'list',
+          message: "The total for your order of (" + lookup.quantity + ") " + product + " is $" + totalCost + "\n\nDo you confirm the accuracy of this order and wish to proceed with this purchase?",
+          choices: ['Yes', 'No'],
+          name: 'confirmOrder'
+        }
+      ]).then(function(answers) {
+        if (answers.confirmOrder === 'Yes'){
+          lookup.placeOrder();
+        }
+        else {
+
+        }
+      });
+    }
+  },
+  placeOrder: function(){
+
   }
 };
 
-
+// res[index].product_name;
 
